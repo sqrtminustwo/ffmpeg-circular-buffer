dc8baaadfa0f1a72c196b28fa3662083072968d3
mziborov <Maksym.Ziborov@UGent.be>
2026-07-17 20:56:18 +0200

    store instead of exchange

--- src/buffer/buffer.cpp
+++ src/buffer/buffer.cpp
@@ -4,9 +4,9 @@
 void Buffer::set_base(uint8_t *base) { this->base = base; }
 uint8_t *Buffer::get_base() { return base; }
 int Buffer::get_offset() { return offset.load(); }
-void Buffer::set_offset(int offset) { this->offset.exchange(offset); }
+void Buffer::set_offset(int offset) { this->offset.store(offset); }
 size_t Buffer::get_total_size() { return total_size.load(); }
-void Buffer::set_total_size(size_t total_size) { this->total_size.exchange(total_size); }
+void Buffer::set_total_size(size_t total_size) { this->total_size.store(total_size); }
 
 int Buffer::normalize_size(int size) { return std::min(size, (int)total_size); }
 int Buffer::normalize_size_default() { return normalize_size(total_size - offset); }
